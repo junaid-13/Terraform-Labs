417ee6fc74799e51ad520dc6ade7880c0d8931b7
junaid-13 <zufishanjunaid.1999@gmail.com>
2025-05-31 09:10:25 +0530

    Fixed the error related to nat gateway.

--- Day-1/main.tf
+++ Day-1/main.tf
@@ -58,7 +58,7 @@ resource "aws_eip" "nat_gateway_eip" {
 resource "aws_nat_gateway" "nat" {
   depends_on = [aws_subnet.public_subnets]
   allocation_id = aws_eip.nat_gateway_eip.id
-  subnet_id = aws_subnet.public_subnets["Public_subnet_1"].id
+  subnet_id = aws_subnet.public_subnets["public_subnet_1"].id
   tags = {
     Name = "demo-nat-gateway"
   }
